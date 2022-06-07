a8112e5a5efce4275f65ffc873fd3fd724bea2aa
Roberto Luna <robertocgw@gmail.com>
2022-06-07 17:25:49 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWitrhRouter/renderWithRouter';
+import NotFound from '../pages/NotFound';
+
+describe('Teste o componente <NotFound.js />', () => {
+  it('Teste se existe um heading h2 com o texto Page requested not found 😭', () => {
+    renderWithRouter(<NotFound />);
+    const verifyHeading = screen.getByRole('heading', {
+      name: /Page requested not found/i,
+      level: 2,
+    });
+    expect(verifyHeading).toBeInTheDocument();
+  });
+  it('Teste se a página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    renderWithRouter(<NotFound />);
+    const img = screen.getByAltText(
+      /Pikachu crying because the page requested was not found/i,
+    );
+    expect(img).toBeInTheDocument();
+    expect(img.src).toBe(
+      'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif',
+    );
+  });
+});
